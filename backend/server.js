26ba8eeb5acd8da880a6d4c342fe9edf409bbc01
Nikhil Singh <nikhilbangalor@gmail.com>
2023-01-26 14:18:18 +0530

    1st

--- backend/server.js
+++ backend/server.js
@@ -20,9 +20,9 @@ var __dirname = path.resolve();
 // app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-// app.get("*", (req, res) =>
-//   res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
-// );
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
+);
 
 const port = process.env.PORT || 5000;
 
