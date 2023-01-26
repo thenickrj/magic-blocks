d1fbcd4077aedd00fb856500130cbaa3e48d063e
Nikhil Singh <nikhilbangalor@gmail.com>
2023-01-26 14:07:05 +0530

    1st

--- backend/server.js
+++ backend/server.js
@@ -1,104 +1,48 @@
-var express = require("express");
-const cors = require("cors");
-var path = require("path");
-const router = express.Router();
-var dotenv = require("dotenv");
+let express = require("express");
 let http = require("http");
+var path = require("path");
+var dotenv = require("dotenv");
+let router = express.Router();
 
-// require("dotenv").config();
 dotenv.config();
 
 var app = express();
-app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const port = process.env.PORT || 5000;
-
-app.get("/home", function (req, res) {
-  console.log("Work");
-  //   res.setHeader("Content-type", "text/html");
-  let val = "Test";
-  //   res.send({ val: "<div>A<h1>B</h1></div>" });
-  res.json({ val: "Working" });
-});
-
-var __dirname = path.resolve();
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
-app.use(express.static(path.join(__dirname, "/frontend/build")));
-
-app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
-);
-
-const httpServer = http.Server(app);
-
-// Single routing
-app.get("/user", function (req, res, next) {
-  const responseData = {
-    message: "Hello, GFG Learner",
-    articleData: {
-      articleName: "How to send JSON response from NodeJS",
-      category: "NodeJS",
-      status: "published",
-    },
-    endingMessage: "Visit Geeksforgeeks.org for more",
-  };
-  // res.json(responseData);
-  res.json({ project: "Magic Blocks" });
-});
-
-// // Single routing
-// router.route("/user").get(function (req, res, next) {
-//   const responseData = {
-//     message: "Hello, GFG Learner",
-//     articleData: {
-//       articleName: "How to send JSON response from NodeJS",
-//       category: "NodeJS",
-//       status: "published",
-//     },
-//     endingMessage: "Visit Geeksforgeeks.org for more",
-//   };
-//   // res.json(responseData);
-//   res.json({ project: "Magic Blocks" });
+// mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/shopit", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
 // });
 
-router.route("/test").get((req, res) => {
-  console.log("Hi");
-  //   res.json("<div>A<h1>B</h1></div>");
-  const responseData = {
-    message: "Hello, GFG Learner",
-    articleData: {
-      articleName: "How to send JSON response from NodeJS",
-      category: "NodeJS",
-      status: "published",
-    },
-    endingMessage: "Visit Geeksforgeeks.org for more",
-  };
-  res.json(responseData);
-  //
-});
+var __dirname = path.resolve();
+// app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-router.route("/post").get((req, res) => {
-  console.log("Hi");
-  res.send("<div>A<h1>B</h1></div>");
-  //   res.send({ val: "<div>A<h1>B</h1></div>" });
-  //
-});
-
-// router.get(
-//   "/categories",
-//   expressAsyncHandler(async (req, res) => {
-//     const categories = await Product.find().distinct("category");
-//     res.send(categories);
-//   })
+// app.get("*", (req, res) =>
+//   res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
 // );
 
+const port = process.env.PORT || 5000;
+
+app.get("/users", async (req, res) => {
+  // let seller = req.query.seller || "";
+  // let sellerFilter = seller ? { seller } : {};
+
+  // let orders = await Order.find({ ...sellerFilter }).populate("user", "name");
+
+  // res.send(orders);
+  res.send({ project: "Magic Blocks" });
+});
+
 app.use(router);
+
+const httpServer = http.Server(app);
 
 httpServer.listen(port, () => {
   console.log(`Server at http://localhost:${port}`);
 });
-// app.listen(port, function () {
-//   console.log(`Server running on port ${port}`);
+// app.listen(5000, () => {
+//   console.log(`Server at http://localhost:${port}`);
 // });
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function App() {
-  return <div>App</div>;
+  let [data, setData] = useState("");
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch("/users");
+      const json = await response.json();
+      console.log("👍 ", json);
+      if (response.ok) {
+        setData(json);
+      }
+    };
+
+    fetchWorkouts();
+  }, []);
+  return <div>Appd</div>;
 }
 
 export default App;
