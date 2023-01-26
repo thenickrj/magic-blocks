b633ca1881cec111a4917f113d585b08417eccd7
Nikhil Singh <nikhilbangalor@gmail.com>
2023-01-26 13:35:04 +0530

    1st

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,7 @@
-import logo from "./logo.svg";
-import building from "./building.png";
-import "./App.css";
-import { useEffect, useState } from "react";
+import React from "react";
 
 function App() {
-  let [data, setData] = useState("");
-  // useEffect(() => {
-  //   fetch("/user")
-  //     .then((res) => res.json())
-  //     .then((data) => setData(data));
-  // }, []);
-
-  useEffect(() => {
-    const fetchWorkouts = async () => {
-      const response = await fetch("/user");
-      const json = await response.json();
-      console.log("👍 ", json);
-      if (response.ok) {
-        setData(json);
-      }
-    };
-
-    // fetchWorkouts();
-  }, []);
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        {/* <h1>Welcome to {data.project}</h1> */}
-        <img
-          src={building}
-          style={{ height: "200px" }}
-          className="App-"
-          alt="logo"
-        />
-        <p>Coming soon!</p>
-      </header>
-    </div>
-  );
+  return <div>App</div>;
 }
+
 export default App;
